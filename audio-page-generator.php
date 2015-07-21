f8db7a98dcbce16b1c414532b24dc5645cc736f7
DanieL <daniel.supplieth@gmx.de>
2015-07-21 16:25:43 +0200

    JS i18n and bugfixes

--- audio-page-generator.php
+++ audio-page-generator.php
@@ -40,7 +40,8 @@ class AudioPageGeneratorPlugin {
     }
 
 	private function load_textdomain() {
-		load_plugin_textdomain( 'audio-page-generator', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
+		$location = dirname( plugin_basename( __FILE__ )) . '/languages';
+		load_plugin_textdomain( 'audio-page-generator', false, $location ); 
 	}
 
     private function define_constants() {
@@ -80,7 +81,16 @@ class AudioPageGeneratorPlugin {
     
     public function show_audio_page( $atts ) {
         $options = get_option('audio-generator_name');
-        wp_enqueue_script( 'Pagination', plugins_url('/js/pagination.js' , __FILE__ ) );
+		wp_register_script( 'pagination', plugins_url('/js/pagination.js' , __FILE__ ) );
+		// Localize the script with new data
+		$translation_array = array(
+				'next' => __( 'Next', 'audio-page-generator' ),
+				'prev' => __( 'Prev', 'audio-page-generator' ),
+		 		'items' => (string) $options['pagination_sites']
+		);
+		wp_localize_script( 'pagination', 'wp_data', $translation_array );
+		// Enqueued script with localized data.
+		wp_enqueue_script( 'pagination' );
         wp_enqueue_style( 'audio_page_style', plugins_url('/css/audio_page_style.css', __FILE__ )  );
         $upload_dir = $options['upload_dir'];
         $upload_dir_mp3 = $upload_dir . "/*.mp3";
@@ -88,7 +98,9 @@ class AudioPageGeneratorPlugin {
         // new object of our ID3TagsReader class
         $oReader = new ID3TagsReader();
         // passing through located files ..
-        $sList = '<input type="hidden" id="current_page" /><input type="hidden" id="show_per_page" /><div class="audio-generator-view" id="wrapper">';
+		$sList = '<input type="hidden" id="current_page" />'
+				 .'<input type="hidden" id="show_per_page" />'
+				 .'<div id="audio-generator-view">';
 		$site = get_site_url();
         foreach ($files as $sSingleFile) {
             $file_parts = explode( '/', $sSingleFile );
